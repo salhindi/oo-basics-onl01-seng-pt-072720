07551bd7aff099c586a88c911ceff8fe11e0e156
Sara A <calisaraalhindi@gmail.com>
2020-08-15 20:48:02 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -22,7 +22,16 @@ end
 def page_count=(num)
   @page_count = num
 end
+
+def page_count
+  @page_count
+
+def genre=(genre)
+  @genre = genre
 end
 
+def genre
+  @genre
+end
 Book.new("And Then There Were None")
 
