e94cfeb7ef1b53c7d9eb9c2cee93bbfb6c85b0f6
Sara A <calisaraalhindi@gmail.com>
2020-08-15 20:44:52 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,7 +1,18 @@
 class Book
   def initialize(title)
+    @title = title
   end
+  
+  def title= (title)
+    @title= title
+  end
+  
   def title
+    @title
+end
+
+def author= (author)
+  @author =author
 end
 end
 
